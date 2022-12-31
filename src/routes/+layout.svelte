935446acfe13ac77b7d8e7ccc673f49b4a71cab1
Dave <david@fitmentindustries.com>
2022-12-31 12:43:17 -0600

    aorking on file upload form

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 		<meta charset="utf-8" />
 		<link rel="icon" href="%sveltekit.assets%/favicon.png" />
 		<meta name="viewport" content="width=device-width" />
-		<link rel="stylesheet" href="src/lib/style/header.css">
+		<link rel="stylesheet" href="/src/lib/style/header.css">
 	</head>
     <header></header>
 	<body data-sveltekit-preload-data="hover">
--- src/routes/gallery/admin/+page.svelte
+++ src/routes/gallery/admin/+page.svelte
@@ -1,1 +1,83 @@
-<h1>lets gooooo</h1>
+<script>
+
+
+</script>
+<div id="main">
+    <form action="">
+        <div class="row"><label for="album">Album: </label><input type="text" name="album" id="album"></div>
+        <div class="row" id="photo"><input type="file" name="photo" id="photo" multiple><p>Drag your files here or click to browse</p></div>
+        <div class="row" id="submit"><input type="submit" value="upload" multiple></div>    
+    </form>
+</div>
+
+
+
+<style>
+    #main {
+        display: block;
+        max-width: 600px;
+        margin: auto;
+        padding-top: 60px;
+        /* font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif; */
+        /* font-family:Verdana, Geneva, Tahoma, sans-serif; */
+        font-family: tahoma;
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        margin:auto;
+    }
+
+    .row {
+        width: 100%;
+        padding-top: 40px;
+        display: flex;
+
+    }
+    label {
+        font-size: 20px;
+    }
+
+    form p {
+        height: 100%;
+        text-align: center;
+        /* line-height: 170px; */
+        color: rgb(46, 1, 42);
+        font-family: Arial;
+        /* line-height: 50px; */
+        /* position: absolute; */
+        /* top: 50%; */
+        /* left: 50%; */
+        /* margin-top: -100px; */
+        /* margin-left: -250px; */
+        height: 200px;
+        border: 4px dashed rgb(46, 1, 42);
+    }
+
+    #submit {
+        
+    }
+
+    #photo {
+        display: flex;
+        flex-direction:column;
+    }
+    input[type=submit] {
+        margin: auto;
+        
+    }
+
+    input[type=file] {
+        margin: auto;
+        /* position: absolute; */
+        /* margin: 0;
+        padding: 0; */
+        width: 100%;
+        height: 100%;
+        outline: none;
+        opacity: 0;
+    }
+
+
+    
+</style>
