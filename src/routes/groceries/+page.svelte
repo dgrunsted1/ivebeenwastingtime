6fe047d37e3fb2dc00a5a2e9cdb3ea61194505b6
dgrunsted1 <dgrunsted1@gmail.com>
2023-01-27 16:07:11 -0600

    merge ingredients | display list

--- src/routes/groceries/+page.svelte
+++ src/routes/groceries/+page.svelte
@@ -16,17 +16,53 @@
             let ingredient_list_in = element.value.split("\n");
             ingredient_list_in.forEach((element) => {
                 if (is_ingredient(element)) {
-
+                    console.log("here");
                     ingredients[Object.keys(ingredients).length] = {
                         value: get_value(element),
                         unit: get_unit(element),
                         name: get_name(element)
                     };
-                    // 
                 }
             });
         });
         console.log(ingredients);
+        grocery_list = merge_ingredients(ingredients);
+    }
+    
+    const merge_ingredients = (ingredients) => {
+        let output = [];
+        
+        Object.keys(ingredients).forEach(function(i) {
+            let found = false;
+            Object.keys(ingredients).forEach(function(j) {
+                if (i != j && (ingredients[i].name.includes(ingredients[j].name) || ingredients[j].name.includes(ingredients[i].name))){
+                    found = true;
+                    if (ingredients[i].unit == ingredients[j].unit){
+                        output.push({
+                            value: ingredients[i].value + ingredients[j].value,
+                            unit: ingredients[i].unit,
+                            name: (ingredients[i].name.includes(ingredients[j].name)) ? ingredients[i].name : ingredients[j].name
+                        });
+                    }else {
+                        output.push({
+                            value: ingredients[i].value + " " +ingredients[i].unit + " and " + ingredients[j].value + " " +ingredients[j].unit,
+                            unit: false,
+                            name: (ingredients[i].name.includes(ingredients[j].name)) ? ingredients[i].name : ingredients[j].name
+                        });
+                    }
+                    ingredients.splice(i, 1);
+                    ingredients.splice(j, 1);
+                }
+            });
+            if (!found) {
+                output.push({
+                    value: ingredients[i].value,
+                    unit: ingredients[i].unit,
+                    name: ingredients[i].name
+                });
+            }
+        });
+        return output;
     }
 
     const is_ingredient = (ingredient_string) => {
@@ -39,12 +75,12 @@
         }
         return false;
     }
+
     const get_unit = (ingredient_string) => {
         let non_units = ["medium", "large", "small", "recipe", "white", "yellow", "white or yellow", "soft"];
         if (ingredient_string.trim().substring(0, 1).match(/\d/)) {
             ingredient_string = ingredient_string.trim().substring(ingredient_string.indexOf(" ")).trim();
             let unit = ingredient_string.substring(0, ingredient_string.indexOf(" "));
-            // console.log("unit",unit);
             if (!non_units.includes(unit)) {
                 return unit;
             }else return "none";
@@ -53,7 +89,6 @@
     }
 
     const get_name = (ingredient_string) => {
-        console.log("get_name ingredient_string",ingredient_string);
         if (ingredient_string.trim().substring(0, 1).match(/\d/)) {
             ingredient_string = ingredient_string.trim();
             ingredient_string = ingredient_string.substring(ingredient_string.indexOf(" ")).trim();
@@ -62,6 +97,14 @@
             return name.replace(/\([^()]*\)/g, '').trim();
         }
         return ingredient_string
+    }
+
+    const display = (ingredient) => {
+        if ([false, "none"].includes(ingredient.unit)){
+            return `${ingredient.value} ${ingredient.name}`;
+        } else {
+            return `${ingredient.value} ${ingredient.unit} ${ingredient.name}`;
+        }
     }
 </script>
 
@@ -77,7 +120,7 @@
     </div>
     <div id="grocery_list" class="column">
         {#each grocery_list as curr}
-            <p class="list_item">{curr}</p>
+            <p class="list_item">{display(curr)}</p>
         {/each}
     </div>
 </div>
@@ -92,7 +135,7 @@
 
     .column {
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         width: 45%;
     }
 
