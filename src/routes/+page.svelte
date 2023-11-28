82c5f5ce848a1d71ed5884d61e0c9dfe7eb8aab2
dgrunsted1 <dgrunsted1@gmail.com>
2023-11-27 22:21:42 -0600

    test deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@ export let data;
 <div id="main" class="w-full flex flex-col">
     <div id="hero" style="background-image: url('{data.hero_url}')" class="flex flex-col relative w-full h-screen m-auto bg-cover bg-no-repeat bg-center">
         <div id="web_title" class="m-auto cursor-default text-5xl text-white">
-            <h1>ivebeenwastingtime</h1>
+            <h1>ivebeenwastingtime test</h1>
         </div>
     </div>
     <div id="content">
