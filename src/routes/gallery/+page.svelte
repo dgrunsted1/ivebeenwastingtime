6e5dc13fc2b5ebb3d9c65151652748dd9586003b
Dave <david@fitmentindustries.com>
2023-12-16 17:11:08 -0600

    margin-auto?

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -28,7 +28,7 @@
 
 <div id="main" class="flex flex-auto flex-wrap justify-around items-center space-y-96 py-64 h-full px-auto">
     {#each data.photos as url}
-    <div class="photo w-96 bg-base-200">
+    <div class="photo w-96 bg-base-200 mx-auto">
         <img src={url} alt="Shoes" class="rounded-lg"/>
       </div>
     {/each}
