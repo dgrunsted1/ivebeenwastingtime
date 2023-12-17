4b06978ae1242abf58fa598114b9af0f5c10e141
dgrunsted1 <dgrunsted1@gmail.com>
2023-12-16 21:58:35 -0600

    try 32 padding

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -4,7 +4,7 @@
    
 </script>
 
-<div id="main" class="flex flex-auto flex-wrap justify-around items-center space-y-96 lg:space-y-12 py-64 lg:py-0 h-full">
+<div id="main" class="flex flex-auto flex-wrap justify-around items-center space-y-96 lg:space-y-12 py-32 lg:py-0 h-full">
     {#each data.photos as url}
     <div class="photo w-96 bg-base-200">
         <img src={url} alt="Shoes" class="rounded-lg" />
