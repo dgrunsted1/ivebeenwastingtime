387cd4937d05a570cdae74cc8ef12cbc6264236c
dgrunsted1 <dgrunsted1@gmail.com>
2023-12-31 11:02:51 -0600

    most of work done need url id still

--- src/routes/cook_recipe/[recipe]/[servings]/+page.js
+++ src/routes/cook_recipe/[recipe]/[servings]/+page.js
@@ -5,9 +5,13 @@ import { currentUser, pb } from '/src/lib/pocketbase.js';
 export async function load({ params }) {
     console.log(params.recipe);
     console.log(params.servings);
-    const result_list = await pb.collection('recipes').getOne(params.recipe, {
-        expand: `notes,ingr_list`
+    const result_list = await pb.collection('recipes').getFirstListItem(`title="${params.recipe}"`, {
+        expand: 'notes,ingr_list'
     });
+    // const result_list = await pb.collection('recipes').getOne(params.recipe, {
+    //     expand: `notes,ingr_list`
+    // });
+    // console.log(result_list);
     console.log(result_list.expand.ingr_list.length);
     console.log(result_list.expand);
     return {
