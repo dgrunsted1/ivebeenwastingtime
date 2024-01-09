40819e4799d91d70c4ff8fcba42a9ab32a2ff7ce
Dave <david@fitmentindustries.com>
2024-01-09 15:19:23 -0600

    homepage mobile

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,30 +4,26 @@ export let data;
 
 <div id="main" class="w-full flex flex-col">
     <div id="hero" style="background-image: url('{data.hero_url}')" class="flex flex-col relative w-full h-screen m-auto bg-cover bg-no-repeat bg-center">
-        <div id="web_title" class="m-auto cursor-default text-5xl text-white">
+        <div id="web_title" class="m-auto cursor-default md:text-5xl text-white">
             <h1>ivebeenwastingtime</h1>
         </div>
     </div>
     <div id="content">
-        <div class="row flex justify-evenly items-center m-24">
-            <a href="/gallery" class="btn btn-primary btn-lg">gallery</a>
-            <p class="text-2xl">checkout my Gallery of photos</p>
+        <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
+            <p class="md:text-2xl">checkout my Gallery of photos</p>
+            <a href="/gallery" class="btn btn-primary md:btn-lg">gallery</a>
         </div>
-        <div class="row flex justify-evenly items-center m-24">
-            <p class="text-2xl">Easily create grocery lists from recipes you find on the internet.</p>
-            <a href="/prep" class="btn btn-primary btn-lg">prep</a>
+        <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
+            <a href="/menu" class="btn btn-primary md:btn-lg">create menu</a>
+            <p class="md:text-2xl">Create menus from recipes youve added</p>
         </div>
-        <div class="row flex justify-evenly items-center m-24">
-            <a href="/menu" class="btn btn-primary btn-lg">create menu</a>
-            <p class="text-2xl">Create menus from recipes youve added</p>
+        <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
+            <p class="md:text-2xl">Check out this weeks recipes and cook.</p>
+            <a href="/today" class="btn btn-primary md:btn-lg">today</a>
         </div>
-        <div class="row flex justify-evenly items-center m-24">
-            <p class="text-2xl">Check out this weeks recipes and cook.</p>
-            <a href="/today" class="btn btn-primary btn-lg">today</a>
-        </div>
-        <div class="row flex justify-evenly items-center m-24">
-            <a href="/my_menus" class="btn btn-primary btn-lg">My Menus</a>
-            <p class="text-2xl">Look at your past menus and choose one for today.</p>
+        <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
+            <a href="/my_menus" class="btn btn-primary md:btn-lg">My Menus</a>
+            <p class="md:text-2xl">Look at your past menus and choose one for today.</p>
         </div>
     </div>
     
