51da5b85f5f85fcb6a5139c4f40e5c3b2bc4b232
Dave <david@fitmentindustries.com>
2024-01-13 09:32:29 -0600

    add add recipe link to the homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,12 +18,16 @@ export let data;
             <p class="md:text-2xl">Create menus from recipes youve added</p>
         </div>
         <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
-            <p class="md:text-2xl">Check out this weeks recipes and cook.</p>
+            <p class="md:text-2xl">Check out this weeks recipes and cook</p>
             <a href="/today" class="btn btn-primary md:btn-lg">today</a>
         </div>
         <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
             <a href="/my_menus" class="btn btn-primary md:btn-lg">My Menus</a>
-            <p class="md:text-2xl">Look at your past menus and choose one for today.</p>
+            <p class="md:text-2xl">Look at your past menus and choose one for today</p>
+        </div>
+        <div class="flex justify-evenly items-center m-5 md:m-24 space-x-3">
+            <p class="md:text-2xl">Add recipes here</p>
+            <a href="/add_recipe" class="btn btn-primary md:btn-lg">add recipe</a>
         </div>
     </div>
     
