0e2f60e07799ce958d9e05c06062addad1386060
Dave <david@fitmentindustries.com>
2024-03-02 09:41:50 -0600

    fix color for ios

--- src/lib/components/alerts.svelte
+++ src/lib/components/alerts.svelte
@@ -5,8 +5,10 @@
     export let type;
     export let show;
     let delay_timer;
+    $: alert_type = "";
 
     afterUpdate(() => {
+        alert_type = `alert-${type}`;;
         if (show) {            
             // if (type !== "error"){
             //     clearTimeout(delay_timer);
@@ -21,7 +23,7 @@
 </script>
 
 
-<div role="alert" id="alert" class="drop-shadow-xl alert alert-{type} fixed top-24 transition-[left] {show ? 'left-0' : '-left-96'} duration-700 p-1 md:p-10 w-96">
+<div role="alert" id="alert" class="drop-shadow-xl alert {alert_type} fixed top-24 transition-[left] {show ? 'left-0' : '-left-96'} duration-700 p-1 md:p-10 w-96">
     {#if type == "warning"}
             <svg xmlns="http://www.w3.org/2000/svg" class="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
     {:else if type == "error"}
