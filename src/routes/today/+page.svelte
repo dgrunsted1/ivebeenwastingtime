7db1943dbf7b2151b90017134ced8b61e0b997c2
dgrunsted1 <dgrunsted1@gmail.com>
2024-03-10 10:43:18 -0500

    styling/ordering today's recipes

--- src/routes/today/+page.svelte
+++ src/routes/today/+page.svelte
@@ -26,6 +26,7 @@
         });
         if (result_list.items[0]){
             todays_menu = result_list.items[0];
+            todays_menu.expand.recipes = todays_menu.expand.recipes.sort(sort_by_made);
             if (!todays_menu.expand.grocery_list || !todays_menu.expand.grocery_list.list){
                 grocery_list = get_grocery_list(todays_menu);
                 grocery_list_id = create_grocery_list(grocery_list, todays_menu.id);
@@ -46,6 +47,14 @@
         }
         loading = false;
     });
+
+    function sort_by_made(a, b){
+        if (a.made == b.made){
+            return 0;
+        }else {
+            return a.made ? 1 : -1;
+        }
+    }
 
     async function update_groceries(e){
         grocery_list_status = "updating";
@@ -100,7 +109,7 @@
                 <div id="recipes" class="">
                     {#each todays_menu.expand.recipes as curr, i}
                     <div on:click={window.location = `/cook_recipe/${curr.url_id}/${todays_menu.servings[curr.id]}`}>
-                        <div class="card card-bordered sm:card-side bg-base-200 shadow-xl max-h-24 my-1.5 mx-1">
+                        <div class="card card-bordered sm:card-side {(todays_menu.made && todays_menu.made[curr.id]) ? "bg-base-300" : "bg-base-200"} shadow-xl max-h-24 my-1.5 mx-1">
                             <figure class="md:w-3/5 {(todays_menu.made && todays_menu.made[curr.id]) ? "blur-sm" : ""}"><img src={curr.image} alt={curr.title}/></figure>
                             <div class="card-body max-h-full flex flex-row p-2 items-center w-full">
                                 <p id={i} class="w-1/2 text-xs">{curr.title}</p>
