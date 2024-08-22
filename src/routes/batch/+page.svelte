02bc872ca17b32b638fec3e48d9d8367d4f1fb9a
dgrunsted1 <dgrunsted1@gmail.com>
2024-08-21 19:34:14 -0500

    finish batch for migrating menus

--- src/routes/batch/+page.svelte
+++ src/routes/batch/+page.svelte
@@ -126,9 +126,8 @@
                 "favorite": recipes[i].favorite,
                 "active": true
             };
-            console.log({data})
+            console.log('recipes_strict', i, recipes.length, data)
             const new_strict_recipe = await pb.collection('recipes_strict').create(data);
-            console.log(new_strict_recipe);
             if (new_strict_recipe.code > 200){
                 console.log("error uploading recipe");
             } else {
@@ -140,12 +139,12 @@
                     }
                     const ingr_data = {
                         "name": ingr.ingredient,
-                        "qty": ingr.quantity,
+                        "qty": validate_number(ingr.quantity),
                         "unit": ingr.unit,
                         "active": true,
                         "recipe": new_strict_recipe.id
                     };
-                    console.log({ingr_data})
+                    console.log('ingredients_strict', j, recipes[i].expand.ingr_list.length, ingr_data)
                     const new_ingr = await pb.collection('ingredients_strict').create(ingr_data);
                     if (new_ingr.code > 200){
                         console.log("error uploading ingr");
@@ -166,64 +165,169 @@
         }
     }
 
-    const migrate_menus = async function(){
+    const migrate_menus = async function(e){
         const menus = await pb.collection('menus').getFullList({
             sort: '-created',
         });
+        e.srcElement.textContent = `Migrating... 0%`;
         for (let i = 0; i < menus.length; i++){
             let recipe_id_match = {};
+            let recipe_ids = [];
             for (let j = 0; j < menus[i].recipes.length; j++){
-                
+                console.log()
+                let recipe_match = await get_new_recipe(menus[i].recipes[j]);
+                recipe_id_match[menus[i].recipes[j]] = recipe_match;
+                recipe_ids.push(recipe_match.id)
             }
+
+            const servings = build_servings(menus[i].servings, recipe_id_match);
+            
+            let made = {};
+            for (let j = 0; j < recipe_ids.length; j++){
+                made[recipe_ids[j]] = false;
+            }
+
+            const sub_recipes = await build_sub_recipes(menus[i].sub_recipes, recipe_id_match);
+
+            const grocery_list = await build_grocery_list(recipe_id_match);
+
+
             const new_menu_data = {
                 "title": menus[i].title,
                 "owner": menus[i].user,
                 "shared_users": [],
                 "active": true,
                 "todays_menu": menus[i].today,
-                "servings": build_servings(menus[i].servings),
+                "servings": servings,
                 "description": menus[i].description,
-                "notes": validate_notes(menus[i].notes),
-                "grocery_list": "RELATION_RECORD_ID",
-                "made": "JSON",
-                "sub_recipes": [
-                    "RELATION_RECORD_ID"
-                ],
-                "recipes": [
-                    "RELATION_RECORD_ID"
-                ]
+                "notes": {},
+                "grocery_list": grocery_list,
+                "made": made,
+                "sub_recipes": sub_recipes,
+                "recipes": recipe_ids
             };
-            console.log({new_menu_data})
-            // const new_menus = await pb.collection('menus_strict').create(new_menu_data);
+            console.log('menus_strict', i, menus.length, new_menu_data)
+            const new_menu = await pb.collection('menus_strict').create(new_menu_data);
+
+            const groc_list_check = await pb.collection('grocery_lists').getOne(grocery_list, {});
+
+            const menu_check = await pb.collection('menus_strict').getOne(new_menu.id, {});
+
+            const updated_menu = await pb.collection('grocery_lists').update(grocery_list, {
+                "menu": new_menu.id
+            });
+            e.srcElement.textContent = `Migrating... ${Math.floor(((i+1)/menus.length)*100)}%`;
         }
+        e.srcElement.textContent = `Done Migrating`;
     }
 
-    const build_grocery_list = function(in_list){
+    const build_sub_recipes = async function(in_sub_recipes, match){
+        let out_sub_recipes = [];
+        for (let key in in_sub_recipes){
 
+            if (!in_sub_recipes[key].ingr_id || !in_sub_recipes[key].recipe_id){
+                continue;
+            }
+
+            const new_ingr = get_new_ingredient(in_sub_recipes[key].ingr_id);
+            const sub_recipe_data = {
+                "ingr": new_ingr.id,
+                "active": true,
+                "parent": match[key].id,
+                "recipe": match[in_sub_recipes[key].recipe_id].id
+            };
+            console.log('sub_recipes', sub_recipe_data);
+            const record = await pb.collection('sub_recipes').create(sub_recipe_data);
+            out_sub_recipes.push(record.id);
+        }
+        return out_sub_recipes;
     }
 
-    const build_servings = function(in_serv){
-
+    const build_grocery_list = async function(recipes_in){
+        // compile list of ingredients
+        let grocery_items = [];
+        let output = "";
+        for (let key in recipes_in){
+            for (let j = 0; j < recipes_in[key].expand.ingr_list.length; j++){
+                const ingr = recipes_in[key].expand.ingr_list[j];
+                if (ingr.name == ""){
+                    continue;
+                }
+                const ingr_data = {
+                    "qty": validate_number(ingr.qty),
+                    "unit": ingr.unit,
+                    "unit_plural": ingr.unit_plural,
+                    "name": ingr.name,
+                    "checked": false,
+                    "ingrs": [
+                        ingr.id
+                    ],
+                    "active": true
+                };
+                console.log('grocery_items', ingr_data)
+                const record = await pb.collection('grocery_items').create(ingr_data);
+                grocery_items.push(record.id);
+            }
+        }
+        const data = {
+            "items": grocery_items,
+            "active": true
+        };
+        console.log('grocery_lists', data)
+        const list_record = await pb.collection('grocery_lists').create(data);
+        output = list_record.id;
+        return output;
     }
 
-    const validate_notes = function(in_notes){
+    const validate_number = function(in_val){
+        if (typeof in_val == "float" || typeof in_val == "int" || typeof in_val == "double"){
+            return in_val;
+        }
+        return parseFloat(in_val);
+    }
 
+    const build_servings = function(in_serv, match){
+        let out_serv = {};
+        for (let key in in_serv){
+            if (!match[key]){
+                continue;
+            }
+            out_serv[match[key].id] = in_serv[key];
+        }
+        return out_serv;
     }
 
     const get_new_recipe = async function(in_id){
         const old_recipe = await pb.collection('recipes').getOne(in_id, {
         });
-
         const new_recipe = await pb.collection('recipes_strict').getList(1, 50, {
             filter: `title = "${old_recipe.title}"`,
+            expand: `ingr_list`
         });
-        if (new_recipe.length == 1){
-            return new_recipe[0];
-        }else if (!new_recipe.length){
+        if (new_recipe.items.length == 1){
+            return new_recipe.items[0];
+        }else if (!new_recipe.items.length){
             console.log(`no recipe match found for ${old_recipe.title}`);
         } else {
             // more than one with this title
             console.log({new_recipe});
+        }
+    }
+
+    const get_new_ingredient = async function(in_id){
+        const old_ingr = await pb.collection('recipes').getOne(in_id, {
+        });
+
+        const new_ingr = await pb.collection('recipes_strict').getList(1, 50, {
+            filter: `name = "${old_ingr.ingredient}" && unit = "${old_ingr.unit}" && qty = ${old_ingr.quantity}`,
+        });
+        if (new_ingr.items.length == 1){
+            return new_ingr.items[0];
+        }else if (!new_ingr.length){
+            console.log(`no recipe match found for ${old_ingr.ingredient}`);
+        } else {
+            // more than one with this title
+            console.log({new_ingr});
         }
     }
 
@@ -300,16 +404,29 @@
     //     }
     // }
 
-    const delete_table = async function (table) {
+    const delete_table = async function (e, table) {
+        
         if (confirm(`are you sure you want to delete all the records in ${table}`)){
             const records = await pb.collection(table).getFullList({
                 sort: '-created',
             });
+            const count = records.length;
+            let percent_done = 0;
+            e.srcElement.textContent = `deleting... ${percent_done}%`;
             for (let i = 0; i < records.length; i++){
+
                 if (records[i] && records[i].id){
                     await pb.collection(table).delete(records[i].id);
                 }
+                if (i > 0){
+                    
+                    percent_done = Math.round((i / count) * 100);
+                    console.log(percent_done, i, count);
+                }
+                e.srcElement.textContent = `deleting... ${percent_done}%`;
             }
+            e.srcElement.textContent = `${table} deleted`;
+            e.srcElement.disabled = true;
         }
     }
 </script>
@@ -335,12 +452,16 @@
                     fill ingr_num
                 </button> -->
 
+                <button id="compare_parsers_btn" class="btn btn-primary w-56" on:click={migrate_recipes}>
+                    migrate recipes
+                </button>
+
                 <button id="compare_parsers_btn" class="btn btn-primary w-56" on:click={migrate_menus}>
                     migrate menus
                 </button>
                 {#each tables as table}
-                    <button id="compare_parsers_btn" class="btn btn-primary w-56" on:click={() => {delete_table(table)}}>
-                        clear {table}
+                    <button id="compare_parsers_btn" class="btn btn-xs btn-error" on:click={(e) => {delete_table(e, table)}}>
+                        {table}
                     </button>
                 {/each}
                 <div>
